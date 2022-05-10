53593e0aee09e3f6ab3db93fbc020e49e8d7d19d
rocco <rocco.cappalla@gmail.com>
2022-05-10 18:40:13 +0000

    update settings

--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -77,7 +77,7 @@ import os
 DATABASES = {
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/db/db.sqlite3',
     },
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
